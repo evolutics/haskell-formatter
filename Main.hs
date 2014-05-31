6535bd0a16e44037dfef5a1b1defd6192c7ef3ef
Benjamin Fischer <benjamin.fischer@evolutics.info>
2014-05-31 18:21:48 +0000

    Realize code formatting as transformation separate from printing

--- Main.hs
+++ Main.hs
@@ -3,7 +3,7 @@ import qualified Data.List as List
 import qualified Language.Haskell.Exts.Annotated as Annotated
 import qualified System.Environment as Environment
 import qualified System.IO as IO
- 
+
 main :: IO ()
 main
   = do programArguments <- Environment.getArgs
@@ -13,28 +13,28 @@ main
              where formatFile filename
                      = readFormatWrite (readFile filename) (Just filename)
                          (writeFile filename)
- 
+
 readFormatWrite ::
                 IO String -> Maybe String -> (String -> IO ()) -> IO ()
 readFormatWrite read maybeSourceName write
   = do source <- read
        let formattedSource = formatSource maybeSourceName source in
          either (IO.hPutStrLn IO.stderr) write formattedSource
- 
+
 formatSource :: Maybe String -> String -> Either String String
 formatSource maybeSourceName
-  = format . Annotated.parseFileContentsWithComments parseMode
-  where format (Annotated.ParseFailed location message)
+  = checkToFormat . Annotated.parseFileContentsWithComments parseMode
+  where checkToFormat (Annotated.ParseFailed location message)
           = Left $ showSourceLocation location message
-        format (Annotated.ParseOk (syntaxTree, comments))
-          = Right $ Annotated.prettyPrint syntaxTree
+        checkToFormat (Annotated.ParseOk parseTree)
+          = Right . show . format $ parseToSourceTree parseTree
         parseMode
           = case maybeSourceName of
                 Nothing -> Annotated.defaultParseMode
                 Just
                   sourceName -> Annotated.defaultParseMode{Annotated.parseFilename =
                                                              sourceName}
- 
+
 showSourceLocation :: Annotated.SrcLoc -> String -> String
 showSourceLocation location message
   = showLocation location ++ majorSeparator ++ message
@@ -43,3 +43,26 @@ showSourceLocation location message
               filename : map show [line, column]
         minorSeparator = ":"
         majorSeparator = ": "
+
+format :: SourceTree -> SourceTree
+format SourceTree{sourceElement = element}
+  = SourceTree{sourceElement = formattedElement,
+               sourceComments = formattedComments}
+  where formattedElement
+          = Annotated.fromParseResult . Annotated.parseFileContents $
+              Annotated.prettyPrint element
+        formattedComments = []
+
+data SourceTree = SourceTree{sourceElement ::
+                             Annotated.Module Annotated.SrcSpanInfo,
+                             sourceComments :: [Annotated.Comment]}
+
+instance Show SourceTree where
+        show SourceTree{sourceElement = element, sourceComments = comments}
+          = Annotated.exactPrint element comments
+
+parseToSourceTree ::
+                  (Annotated.Module Annotated.SrcSpanInfo, [Annotated.Comment]) ->
+                    SourceTree
+parseToSourceTree (element, comments)
+  = SourceTree{sourceElement = element, sourceComments = comments}
