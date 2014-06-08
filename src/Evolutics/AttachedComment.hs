38e611531869b42ca46d91e1d45a386f74fca6e9
Benjamin Fischer <benjamin.fischer@evolutics.info>
2014-06-08 14:18:55 +0200

    Design code formatting with comments using types

--- src/Evolutics/AttachedComment.hs
+++
@@ -1,7 +0,0 @@
-module Evolutics.AttachedComment (AttachedComment) where
-import qualified Language.Haskell.Exts.Annotated as Exts
-
-data AttachedComment = AttachedComment Displacement Exts.Comment
-
-data Displacement = Before
-                  | After
--- src/Evolutics/Formatter.hs
+++ src/Evolutics/Formatter.hs
@@ -1,47 +1,59 @@
-{-# LANGUAGE ScopedTypeVariables #-}
 module Evolutics.Formatter (formatSource) where
-import qualified Data.Data as Data
-import qualified Data.Generics as Generics
-import qualified Debug.Trace as Trace
 import qualified Language.Haskell.Exts.Annotated as Exts
-import qualified Evolutics.SourceTree as SourceTree
 import qualified Evolutics.Tools as Tools
+
+data ConcreteCommented = ConcreteCommented (Exts.Module
+                                              Exts.SrcSpanInfo)
+                                           [Exts.Comment]
+
+data ConcreteCommentless = ConcreteCommentless (Exts.Module
+                                                  Exts.SrcSpanInfo)
+
+data Abstract = Abstract (Exts.Module [AbstractComment])
+
+data AbstractComment = AbstractComment Displacement Bool String
+
+data Displacement = Before
+                  | After
 
 formatSource :: Maybe FilePath -> String -> Either String String
 formatSource maybeFile
   = format . Exts.parseFileContentsWithComments parseMode
   where format (Exts.ParseFailed location message)
           = Left $ Tools.formatSourceMessage location message
-        format (Exts.ParseOk (element, comments))
-          = Right . show . formatTree $ attachComments element' comments
-          where element' = Trace.trace (show $ visit element) element
+        format (Exts.ParseOk (root, comments))
+          = Right . rawFormat $ ConcreteCommented root comments
         parseMode
           = case maybeFile of
                 Nothing -> Exts.defaultParseMode
                 Just file -> Exts.defaultParseMode{Exts.parseFilename = file}
 
-formatTree :: SourceTree.SourceTree -> SourceTree.SourceTree
-formatTree sourceTree = fromElementOnly element
-  where element
-          = Exts.fromParseResult . Exts.parseFileContents . Exts.prettyPrint
-              $ SourceTree.toElementOnly sourceTree
+rawFormat :: ConcreteCommented -> String
+rawFormat code = Exts.exactPrint root comments
+  where ConcreteCommented root comments = formatCode code
 
-fromElementOnly ::
-                Exts.Module Exts.SrcSpanInfo -> SourceTree.SourceTree
-fromElementOnly element
-  = SourceTree.SourceTree $ fmap (flip (,) []) element
+formatCode :: ConcreteCommented -> ConcreteCommented
+formatCode concreteCommented
+  = integrateComments abstract concreteCommentless
+  where abstract = assignComments concreteCommented
+        concreteCommentless
+          = arrangeElements $ dropComments concreteCommented
 
-attachComments ::
-               Exts.Module Exts.SrcSpanInfo ->
-                 [Exts.Comment] -> SourceTree.SourceTree
-attachComments element _ = fromElementOnly element
+integrateComments ::
+                  Abstract -> ConcreteCommentless -> ConcreteCommented
+integrateComments _ (ConcreteCommentless root)
+  = ConcreteCommented root []
 
-visit :: (Data.Data a) => a -> [String]
-visit = Generics.everything (++) query
+assignComments :: ConcreteCommented -> Abstract
+assignComments (ConcreteCommented root _)
+  = Abstract $ fmap (const []) root
 
-query :: (Data.Data a) => a -> [String]
-query = Generics.mkQ [] select
-  where select
-          (Exts.Ident _ identifier :: Exts.Name Exts.SrcSpanInfo)
-          = [identifier]
-        select _ = []
+arrangeElements :: ConcreteCommentless -> ConcreteCommentless
+arrangeElements (ConcreteCommentless root)
+  = ConcreteCommentless .
+      Exts.fromParseResult . Exts.parseFileContents
+      $ Exts.prettyPrint root
+
+dropComments :: ConcreteCommented -> ConcreteCommentless
+dropComments (ConcreteCommented root comments)
+  = ConcreteCommentless root
