c668cc22444b3c2cc98008357ee19ff0b604ab30
Benjamin Fischer <benjamin.fischer@evolutics.info>
2014-06-09 21:00:51 +0200

    Introduce creator for abstract comment

--- src/Evolutics/Code/Abstract.hs
+++ src/Evolutics/Code/Abstract.hs
@@ -1,14 +1,20 @@
-module Evolutics.Code.Abstract (Code, Comment, createCode) where
+module Evolutics.Code.Abstract
+       (Code, Comment, Displacement, createCode, createComment) where
 import qualified Language.Haskell.Exts.Annotated as Exts
 import qualified Evolutics.Code.Concrete as Concrete
 
 data Code = Code{root :: Exts.Module [Comment]}
 
-data Comment = Comment{displacement :: Displacement,
-                       isMultiLine :: Bool, content :: String}
+data Comment = Comment{commentDisplacement :: Displacement,
+                       isCommentMultiLine :: Bool, commentContent :: String}
 
 data Displacement = Before
                   | After
 
 createCode :: Exts.Module [Comment] -> Code
 createCode root = Code{root = root}
+
+createComment :: Displacement -> Bool -> String -> Comment
+createComment displacement isMultiLine content
+  = Comment{commentDisplacement = displacement,
+            isCommentMultiLine = isMultiLine, commentContent = content}
