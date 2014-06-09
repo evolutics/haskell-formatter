80735f6ba1a0265f7a9f94f8b7bd6453bbab8a38
Benjamin Fischer <benjamin.fischer@evolutics.info>
2014-06-09 18:17:33 +0200

    Rename types for abstract code

--- src/Evolutics/Code/Abstract.hs
+++ src/Evolutics/Code/Abstract.hs
@@ -1,14 +1,13 @@
-module Evolutics.Code.Abstract.Abstract (Abstract, create) where
+module Evolutics.Code.Abstract (Code, Comment, create) where
 import qualified Language.Haskell.Exts.Annotated as Exts
 
-data Abstract = Abstract{root :: Exts.Module [AbstractComment]}
+data Code = Code{root :: Exts.Module [Comment]}
 
-data AbstractComment = AbstractComment{displacement ::
-                                       Displacement,
-                                       isMultiLine :: Bool, content :: String}
+data Comment = Comment{displacement :: Displacement,
+                       isMultiLine :: Bool, content :: String}
 
 data Displacement = Before
                   | After
 
-create :: Exts.Module [AbstractComment] -> Abstract
-create root = Abstract{root = root}
+create :: Exts.Module [Comment] -> Code
+create root = Code{root = root}
