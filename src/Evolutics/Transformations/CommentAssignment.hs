a2d6d403716905465a74853a0477097019701580
Benjamin Fischer <benjamin.fischer@evolutics.info>
2014-06-09 20:08:05 +0200

    Select concrete comments during traversal

--- src/Evolutics/Transformations/CommentAssignment.hs
+++ src/Evolutics/Transformations/CommentAssignment.hs
@@ -4,6 +4,7 @@ import qualified Data.Traversable as Traversable
 import qualified Language.Haskell.Exts.Annotated as Exts
 import qualified Evolutics.Code.Abstract as Abstract
 import qualified Evolutics.Code.Concrete as Concrete
+import qualified Evolutics.Tools.Core as Core
 
 assignComments :: Concrete.Commented -> Abstract.Code
 assignComments concrete = Abstract.create root'
@@ -17,5 +18,14 @@ processElement ::
                  Exts.SrcSpanInfo -> ([Exts.Comment], [Abstract.Comment])
 processElement concreteComments elementPortion
   = (remainder, abstractComments)
-  where remainder = concreteComments
+  where (choice, remainder)
+          = span (follows elementPortion) concreteComments
         abstractComments = []
+
+follows :: Exts.SrcSpanInfo -> Exts.Comment -> Bool
+follows portion comment = comparePortions portion comment == GT
+
+comparePortions :: Exts.SrcSpanInfo -> Exts.Comment -> Ordering
+comparePortions portion comment
+  = Core.comparePortions (Exts.srcInfoSpan portion) $
+      Concrete.commentPortion comment
