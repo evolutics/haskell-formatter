e5d100670df568653cf129dc61da84cbbb6d8204
Benjamin Fischer <benjamin.fischer@evolutics.info>
2014-06-09 18:13:31 +0200

    Merge code modules

---
+++ src/Evolutics/Code/Concrete.hs
@@ -0,0 +1,28 @@
+module Evolutics.Code.Concrete
+       (Commented, commentedRoot, comments, Commentless, commentlessRoot,
+        createCommented, dropComments, createCommentless)
+       where
+import qualified Language.Haskell.Exts.Annotated as Exts
+
+data Commented = Commented{commentedRoot ::
+                           Exts.Module Exts.SrcSpanInfo,
+                           comments :: [Exts.Comment]}
+
+data Commentless = Commentless{commentlessRoot ::
+                               Exts.Module Exts.SrcSpanInfo}
+
+instance Show Commented where
+        show Commented{commentedRoot = root, comments = comments}
+          = Exts.exactPrint root comments
+
+createCommented ::
+                Exts.Module Exts.SrcSpanInfo -> [Exts.Comment] -> Commented
+createCommented root comments
+  = Commented{commentedRoot = root, comments = comments}
+
+createCommentless :: Exts.Module Exts.SrcSpanInfo -> Commentless
+createCommentless root = Commentless{commentlessRoot = root}
+
+dropComments :: Commented -> Commentless
+dropComments Commented{commentedRoot = root}
+  = createCommentless root
--- src/Evolutics/Code/Concrete/Commentless.hs
+++
@@ -1,9 +0,0 @@
-module Evolutics.Code.Concrete.Commentless
-       (Commentless, root, create) where
-import qualified Language.Haskell.Exts.Annotated as Exts
-
-data Commentless = Commentless{root ::
-                               Exts.Module Exts.SrcSpanInfo}
-
-create :: Exts.Module Exts.SrcSpanInfo -> Commentless
-create root = Commentless{root = root}
