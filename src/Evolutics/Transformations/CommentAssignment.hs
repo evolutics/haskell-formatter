c8dbf3379726ae104fe45cfddb6846fc9d59fb59
Benjamin Fischer <benjamin.fischer@evolutics.info>
2014-06-09 20:04:33 +0200

    Introduce scaffolding for traversal

--- src/Evolutics/Transformations/CommentAssignment.hs
+++ src/Evolutics/Transformations/CommentAssignment.hs
@@ -1,8 +1,21 @@
 module Evolutics.Transformations.CommentAssignment (assignComments)
        where
+import qualified Data.Traversable as Traversable
+import qualified Language.Haskell.Exts.Annotated as Exts
 import qualified Evolutics.Code.Abstract as Abstract
 import qualified Evolutics.Code.Concrete as Concrete
 
 assignComments :: Concrete.Commented -> Abstract.Code
-assignComments
-  = Abstract.create . fmap (const []) . Concrete.commentedRoot
+assignComments concrete = Abstract.create root'
+  where root = Concrete.commentedRoot concrete
+        comments = Concrete.comments concrete
+        (remainder, root')
+          = Traversable.mapAccumL processElement comments root
+
+processElement ::
+               [Exts.Comment] ->
+                 Exts.SrcSpanInfo -> ([Exts.Comment], [Abstract.Comment])
+processElement concreteComments elementPortion
+  = (remainder, abstractComments)
+  where remainder = concreteComments
+        abstractComments = []
