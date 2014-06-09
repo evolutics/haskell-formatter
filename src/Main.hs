51ebb1d8ed14c61f814982913edf0ba82f5d3298
Benjamin Fischer <benjamin.fischer@evolutics.info>
2014-06-09 16:57:37 +0200

    Change reference according to nested structure

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,9 @@
 module Main (main) where
 import qualified System.Environment as Environment
 import qualified Evolutics.Formatting as Formatting
-import qualified Evolutics.Tools as Tools
+import qualified Evolutics.Tools.Core as Core
 
 main :: IO ()
 main
   = Environment.getArgs >>=
-      Tools.transformFilesOrStandardStreams Formatting.formatSource
+      Core.transformFilesOrStandardStreams Formatting.formatSource
