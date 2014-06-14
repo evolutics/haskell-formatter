2de04eb99a906d7092ffe3d473457567ad3f3fbd
Benjamin Fischer <benjamin.fischer@evolutics.info>
2014-06-14 16:56:49 +0200

    Create tools for repetition until value is present

--- src/Evolutics/Tools/Functions.hs
+++ src/Evolutics/Tools/Functions.hs
@@ -1,4 +1,26 @@
-module Evolutics.Tools.Functions (doubleArgument) where
+module Evolutics.Tools.Functions
+       (doubleArgument, iterateUntil, findJust) where
+import qualified Control.Monad as Monad
+import qualified Data.Foldable as Foldable
+import qualified Data.Maybe as Maybe
 
 doubleArgument :: (a -> a -> b) -> a -> b
 doubleArgument function argument = function argument argument
+
+iterateUntil :: (a -> Maybe a) -> a -> [a]
+iterateUntil function base
+  = base :
+      case function base of
+          Nothing -> []
+          Just next -> iterateUntil function next
+
+findJust ::
+           (Functor t, Foldable.Foldable t) =>
+           (a -> Maybe b) -> t a -> Maybe b
+findJust function = Monad.join . findMap Maybe.isJust function
+
+findMap ::
+          (Functor t, Foldable.Foldable t) =>
+          (b -> Bool) -> (a -> b) -> t a -> Maybe b
+findMap predicate function
+  = Foldable.find predicate . fmap function
