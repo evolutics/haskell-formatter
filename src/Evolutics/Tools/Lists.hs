74b6d1d41358fe0f176efb39c06a2509e4021500
Benjamin Fischer <benjamin.fischer@evolutics.info>
2014-06-14 19:45:59 +0200

    Separate tools related to delimiter splitting

--- src/Evolutics/Tools/Lists.hs
+++ src/Evolutics/Tools/Lists.hs
@@ -1,57 +1,19 @@
-module Evolutics.Tools.Lists () where
-import qualified Data.List as List
-import qualified Data.List.Split.Internals as Internals
+module Evolutics.Tools.Lists
+       (takeEvery, concatenateNeighbors, partitions) where
 import qualified Evolutics.Tools.Functions as Functions
-
-data Splitting a = Splitting{delimiters :: [[a]],
-                             delimiterPolicy :: Internals.DelimPolicy}
-
-split :: (Eq a) => Splitting a -> [a] -> [[a]]
-split splitting list = processedDelimiters
-  where processedDelimiters
-          = case delimiterPolicy splitting of
-                Internals.Drop -> takeEvery period raw
-                Internals.Keep -> raw
-                Internals.KeepLeft -> concatenatePairs raw
-                Internals.KeepRight -> case raw of
-                                           (first : rest) -> first : concatenatePairs rest
-        period = 2
-        raw = rawSplit (delimiters splitting) list
-        concatenatePairs = concatenateNeighbors period
 
 takeEvery :: Int -> [a] -> [a]
 takeEvery _ [] = []
 takeEvery period list@(first : _)
   = first : takeEvery period (drop period list)
 
-rawSplit :: (Eq a) => [[a]] -> [a] -> [[a]]
-rawSplit separators = Functions.untilRight move . (,) []
-  where move (splitLists, restList)
-          = case findFirstSeparator separators restList of
-                Nothing -> Right $ splitLists ++ [restList]
-                Just (left, separator, right) -> Left
-                                                   (splitLists ++ [left, separator], right)
-
-findFirstSeparator ::
-                     (Eq a) => [[a]] -> [a] -> Maybe ([a], [a], [a])
-findFirstSeparator separators
-  = Functions.findJust split . partitions
-  where split (left, right)
-          = fmap (prepend left) $ stripFirstPrefix separators right
-        prepend left (middle, right) = (left, middle, right)
-
-partitions :: [a] -> [([a], [a])]
-partitions list = Functions.iterateUntilNothing move ([], list)
-  where move (_, []) = Nothing
-        move (left, middle : right) = Just (left ++ [middle], right)
-
-stripFirstPrefix :: (Eq a) => [[a]] -> [a] -> Maybe ([a], [a])
-stripFirstPrefix prefixes list = Functions.findJust strip prefixes
-  where strip prefix
-          = fmap ((,) prefix) $ List.stripPrefix prefix list
-
 concatenateNeighbors :: Int -> [[a]] -> [[a]]
 concatenateNeighbors _ [] = []
 concatenateNeighbors period lists
   = concat list : concatenateNeighbors period rest
   where (list, rest) = splitAt period lists
+
+partitions :: [a] -> [([a], [a])]
+partitions list = Functions.iterateUntilNothing move ([], list)
+  where move (_, []) = Nothing
+        move (left, middle : right) = Just (left ++ [middle], right)
---
+++ src/Evolutics/Tools/Splitting.hs
@@ -0,0 +1,42 @@
+module Evolutics.Tools.Splitting () where
+import qualified Data.List as List
+import qualified Data.List.Split.Internals as Internals
+import qualified Evolutics.Tools.Functions as Functions
+import qualified Evolutics.Tools.Lists as Lists
+
+data Splitting a = Splitting{delimiters :: [[a]],
+                             delimiterPolicy :: Internals.DelimPolicy}
+
+split :: (Eq a) => Splitting a -> [a] -> [[a]]
+split splitting list = processedDelimiters
+  where processedDelimiters
+          = case delimiterPolicy splitting of
+                Internals.Drop -> Lists.takeEvery period raw
+                Internals.Keep -> raw
+                Internals.KeepLeft -> concatenatePairs raw
+                Internals.KeepRight -> case raw of
+                                           (first : rest) -> first : concatenatePairs rest
+        period = 2
+        raw = rawSplit (delimiters splitting) list
+        concatenatePairs = Lists.concatenateNeighbors period
+
+rawSplit :: (Eq a) => [[a]] -> [a] -> [[a]]
+rawSplit separators = Functions.untilRight move . (,) []
+  where move (splitLists, restList)
+          = case findFirstSeparator separators restList of
+                Nothing -> Right $ splitLists ++ [restList]
+                Just (left, separator, right) -> Left
+                                                   (splitLists ++ [left, separator], right)
+
+findFirstSeparator ::
+                     (Eq a) => [[a]] -> [a] -> Maybe ([a], [a], [a])
+findFirstSeparator separators
+  = Functions.findJust split . Lists.partitions
+  where split (left, right)
+          = fmap (prepend left) $ stripFirstPrefix separators right
+        prepend left (middle, right) = (left, middle, right)
+
+stripFirstPrefix :: (Eq a) => [[a]] -> [a] -> Maybe ([a], [a])
+stripFirstPrefix prefixes list = Functions.findJust strip prefixes
+  where strip prefix
+          = fmap ((,) prefix) $ List.stripPrefix prefix list
