80871edd403dd48f5c03ed93e53761eaacb565e8
Benjamin Fischer <benjamin.fischer@evolutics.info>
2014-06-15 20:53:34 +0200

    Implement creation of concrete comments

--- src/Evolutics/Code/Concrete.hs
+++ src/Evolutics/Code/Concrete.hs
@@ -1,9 +1,10 @@
 module Evolutics.Code.Concrete
        (Commented, commentedRoot, comments, Commentless, commentlessRoot,
-        createCommented, createCommentless, dropComments,
+        createCommented, createCommentless, dropComments, createComment,
         isCommentMultiLine, commentPortion, commentContent)
        where
 import qualified Language.Haskell.Exts.Annotated as Exts
+import qualified Evolutics.Tools.Newlines as Newlines
 import qualified Evolutics.Tools.SourceLocations as SourceLocations
 
 data Commented = Commented{commentedRoot ::
@@ -28,6 +29,26 @@ createCommentless root = Commentless{commentlessRoot = root}
 dropComments :: Commented -> Commentless
 dropComments Commented{commentedRoot = root}
   = createCommentless root
+
+createComment :: Bool -> String -> Exts.SrcLoc -> Exts.Comment
+createComment isMultiLine content startLocation
+  = Exts.Comment isMultiLine portion content
+  where portion = Exts.mkSrcSpan startLocation endLocation
+        endLocation
+          = Exts.SrcLoc{Exts.srcFilename = file, Exts.srcLine = endLine,
+                        Exts.srcColumn = endColumn}
+        file = Exts.fileName startLocation
+        endLine = startLine + lineCount - 1
+        startLine = Exts.startLine startLocation
+        lineCount = length contentLines
+        contentLines = Newlines.splitSeparatedLines content
+        endColumn = contentEndColumn + if isMultiLine then 2 else 0
+        contentEndColumn
+          = lastContentLineStartColumn + lastContentLineLength - 1
+        lastContentLineStartColumn
+          = if lineCount == 1 then startColumn + 2 else 1
+        startColumn = Exts.startColumn startLocation
+        lastContentLineLength = length $ last contentLines
 
 isCommentMultiLine :: Exts.Comment -> Bool
 isCommentMultiLine (Exts.Comment isMultiLine _ _) = isMultiLine
