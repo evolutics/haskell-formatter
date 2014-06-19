f887e576a7472bf2fe2987eeb29251748fcc1dfc
Benjamin Fischer <benjamin.fischer@evolutics.info>
2014-06-19 21:57:23 +0200

    Create facade to source manipulations for reduced interface

---
+++ src/Evolutics/Code/Core.hs
@@ -0,0 +1,12 @@
+module Evolutics.Code.Core
+       ((Exts.=~=), Exts.amap, Exts.ann, Exts.Comment(..),
+        Exts.defaultParseMode, Exts.exactPrint, Exts.fileName,
+        Exts.fromParseResult, Exts.mkSrcSpan, Exts.Module,
+        Exts.parseFileContents, Exts.parseFileContentsWithComments,
+        Exts.parseFilename, Exts.ParseResult(..), Exts.prettyPrint,
+        Exts.srcInfoPoints, Exts.srcInfoSpan, Exts.SrcLoc(..),
+        Exts.SrcSpan, Exts.srcSpanEnd, Exts.srcSpanEndLine,
+        Exts.srcSpanFilename, Exts.SrcSpanInfo, Exts.srcSpanStart,
+        Exts.srcSpanStartLine, Exts.startColumn, Exts.startLine)
+       where
+import qualified Language.Haskell.Exts.Annotated as Exts
