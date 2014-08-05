011be98d48619cd95ad200e3ab8f39de45ca4c65
Benjamin Fischer <benjamin.fischer@evolutics.info>
2014-08-05 20:57:50 +0200

    Test nested comments

---
+++ testsuite/resources/source/comments/treats_nested_comments/Input.hs
@@ -0,0 +1,4 @@
+predecessor = predecessor
+  {-^ {- inner -} outer -}
+successor :: a
+successor = successor
---
+++ testsuite/resources/source/comments/treats_nested_comments/Output.hs
@@ -0,0 +1,5 @@
+predecessor = predecessor
+{-^ {- inner -} outer -}
+
+successor :: a
+successor = successor
