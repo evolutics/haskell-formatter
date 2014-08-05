9c7057a89e0d1e4021bf4b1f4ff48ecab1c360b9
Benjamin Fischer <benjamin.fischer@evolutics.info>
2014-08-05 18:36:59 +0200

    Ignore changes to elements in arrangement

--- src/library/Language/Haskell/Formatter/Default/ElementArrangement.hs
+++ src/library/Language/Haskell/Formatter/Default/ElementArrangement.hs
@@ -6,16 +6,25 @@ import qualified Language.Haskell.Formatter.Code.Merged as Merged
 import qualified Language.Haskell.Formatter.Code.Source as Source
 import qualified Language.Haskell.Formatter.Error as Error
 import qualified Language.Haskell.Formatter.Result as Result
+import qualified Language.Haskell.Formatter.Toolkit.FunctionTool
+       as FunctionTool
 
 arrangeElements ::
                 Merged.Code -> Result.Result Concrete.Commentless
 arrangeElements merged
-  = case result of
+  = case parseResult of
         Source.ParseFailed _ _ -> Result.fatalError
                                     Error.ElementArrangementError
-        Source.ParseOk code' -> return
-                                  code{Concrete.commentlessRoot = code'}
-  where result
-          = Source.parseFileContents . Source.prettyPrint $
-              Concrete.commentlessRoot code
+        Source.ParseOk arrangedButChanged -> case maybeArrangedOriginal of
+                                                 Nothing -> Result.fatalError
+                                                              Error.ElementArrangementError
+                                                 Just arrangedOriginal -> return
+                                                                            code{Concrete.commentlessRoot
+                                                                                   =
+                                                                                   arrangedOriginal}
+          where maybeArrangedOriginal
+                  = FunctionTool.halfZipWith (flip const) original arrangedButChanged
+  where parseResult
+          = Source.parseFileContents $ Source.prettyPrint original
+        original = Concrete.commentlessRoot code
         code = Merged.makeCommentless merged
