716fc4cfce8e959401612fe957aceef0c66a9860
Benjamin Fischer <benjamin.fischer@evolutics.info>
2019-05-26 12:24:02 +0200

    Order command-line options alphabetically as much as possible

--- format.sh
+++ format.sh
@@ -4,5 +4,5 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-find Setup.hs src testsuite/src -type f -name '*.hs' -print0 \
-  | xargs -n 1 -0 -I {} stack run -- --force --input {} --output {}
+find Setup.hs src testsuite/src -name '*.hs' -type f -print0 \
+  | xargs -0 -I {} -n 1 stack run -- --force --input {} --output {}
